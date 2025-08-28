fd7759c59181bc6b1f6da3864aa37cc8e05b8c9c
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-08-27 22:11:28 -0400

    Created solution to pass all tests in exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    };
+    let combined = ''
+    for (let i = 0; i < num; i++) combined += str;
+    return combined;
 };
 
 // Do not edit below this line
