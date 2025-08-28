70263d63439629a8eb4c774d5e8a7779b9ee3ed0
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-08-27 23:11:21 -0400

    completed exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a > b) {
+        const temp = b;
+        b = a;
+        a = temp;
+    }
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    };
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    };
 
+    for (i = a; i <= b; i++){
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
