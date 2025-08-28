2282b8d9787afd4fea32329fecaf1a0bff755fdc
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-08-27 23:43:04 -0400

    Completed exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const celsius = (num - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const fahr = num * (9 / 5) + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
