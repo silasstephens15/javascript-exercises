a6bfbddd021108ecf370b1052f924f0fe2262152
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-07 19:46:45 -0400

    Completed mulitply function in exercise 12.
    Allows multiplying all values in an array.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -10,7 +10,9 @@ const sum = function (arr) {
   return arr.reduce((prev, num) => prev + num, 0);
 };
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.reduce((prev, num) => prev * num, 1);
+};
 
 const power = function () {};
 
