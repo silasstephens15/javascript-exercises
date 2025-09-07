ffb9d84bade8eb0a6598e5736408f67577d786bb
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-07 19:55:51 -0400

    Completed all tests in exercise 12.
    Factorial function and power function both return expected values.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -14,9 +14,19 @@ const multiply = function (arr) {
   return arr.reduce((prev, num) => prev * num, 1);
 };
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b;
+};
 
-const factorial = function () {};
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  }
+  for (let i = a - 1; i > 0; i--) {
+    a = a * i;
+  }
+  return a;
+};
 
 // Do not edit below this line
 module.exports = {
