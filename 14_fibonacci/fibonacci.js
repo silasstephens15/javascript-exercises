07df9f92941789874058dfbe09e1095340f2de79
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-07 20:38:43 -0400

    Completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  let a = 0;
+  let b = 1;
+  let c = 0;
+  for (let i = 0; i < num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return a;
 };
 
 // Do not edit below this line
