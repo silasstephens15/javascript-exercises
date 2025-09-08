6d96661b6b327440af9713c8e71958e61c7f9fe2
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-07 20:14:31 -0400

    Completed exercise 13.
    Cleaned input string, reversed and compared to input.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let cleanText = text
+    .toLowerCase()
+    .replaceAll("!", "")
+    .replaceAll(",", "")
+    .replaceAll(".", "")
+    .replaceAll(" ", "");
+  let reversed = "";
+  for (let i = cleanText.length - 1; i >= 0; i--) {
+    reversed += cleanText[i];
+  }
+  return reversed === cleanText;
 };
 
 // Do not edit below this line
