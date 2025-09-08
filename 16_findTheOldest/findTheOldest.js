09e19b9be30f1924a0f318a7509318121f9a2142
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-07 21:01:07 -0400

    Passed all tests in exercise 16.
    Created function to find oldest person from array of objects.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let oldest = {};
+  arr.reduce((prevAge, user) => {
+    if (!("yearOfDeath" in user)) {
+      user.yearOfDeath = new Date().getFullYear();
+    }
+    age = user.yearOfDeath - user.yearOfBirth;
+    if (age > prevAge) {
+      oldest = user;
+    }
+    return age;
+  }, 0);
+  return oldest;
 };
 
 // Do not edit below this line
